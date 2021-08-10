1dc93df11aabc99d9d5f26b96be7cb5ccb9f8589
Alexei Raiu <alex.rayu@gmail.com>
2021-08-10 08:51:45 +0300

    Fix single vs multiple value.

--- contacts.py
+++ contacts.py
@@ -30,27 +30,27 @@ def contactData(nid):
         nid = node['nid']
 
         # body
-        value = drupal.field_values(cursor, nid, 'body')
+        value = drupal.field_value(cursor, nid, 'body')
         if value is not None: node['body'] = value
 
         # field_kontaktperson_vorname
-        value = drupal.field_values(cursor, nid, 'field_kontaktperson_vorname')
+        value = drupal.field_value(cursor, nid, 'field_kontaktperson_vorname')
         if value is not None: node['first_name'] = value
 
         # field_kontaktperson_nachname
-        value = drupal.field_values(cursor, nid, 'field_kontaktperson_nachname')
+        value = drupal.field_value(cursor, nid, 'field_kontaktperson_nachname')
         if value is not None: node['second_name'] = value
 
         # field_kontaktperson_titel
-        value = drupal.field_values(cursor, nid, 'field_kontaktperson_titel')
+        value = drupal.field_value(cursor, nid, 'field_kontaktperson_titel')
         if value is not None: node['formal_address'] = value
 
         # field_kontaktperson_telefon
-        value = drupal.field_values(cursor, nid, 'field_kontaktperson_telefon')
+        value = drupal.field_value(cursor, nid, 'field_kontaktperson_telefon')
         if value is not None: node['phone'] = value
 
         # field_kontaktperson_email
-        value = drupal.field_values(cursor, nid, 'field_kontaktperson_email')
+        value = drupal.field_value(cursor, nid, 'field_kontaktperson_email')
         if value is not None: node['email'] = value
 
         cursor.close()
